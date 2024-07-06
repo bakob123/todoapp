03fe670b53106a93bc6904bfdbc391ba81b345e2
“Bako <bencebako123@gmail.com>
2024-07-06 08:52:36 +0200

    Initial commit

---
+++ src/test/java/hu/app/todo/TodoApplicationTests.java
@@ -0,0 +1,11 @@
+package hu.app.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
